aa7bbfcdf6d434af96cd94bd1a827e0e7ed91ebe
Kanishka-08 <111690574+Kanishka-08@users.noreply.github.com>
2022-09-01 17:58:36 +0530

    SI - While loop for 3 repetions

---
+++ SI_While_loop.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int p,n,count;
+    float r,si ;
+
+    count=1;
+    while(count<=3)
+      {
+        printf("Enter values of p,n&r");
+        scanf("%i %i %f",&p,&n,&r);
+
+        si=p*n*r/100;
+
+        printf("Simple interest =\n Rs.%f",si);
+
+        count=count+1;
+
+        
+      }
+      return 0;
+}
